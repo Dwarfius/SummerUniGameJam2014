37f441c3d9c0dc460a4bf80d96ea08250b62916a
ricardobarros95 <ricardobarros95@gmail.com>
2014-09-27 10:07:46 +0100

    MainMenuUpdate1.1

--- GameJamUnityProject/Assets/Scripts/MainMenu.cs
+++ GameJamUnityProject/Assets/Scripts/MainMenu.cs
@@ -18,6 +18,7 @@ public class MainMenu : MonoBehaviour {
 	public Texture2D optionsTexture;
 	public Texture2D closeOptionsButton;
 	public GUIStyle exitButtonStyle;
+	public bool mainMenuButtons = true;
 	// Use this for initialization
 	void Start () {
 	
@@ -28,26 +29,36 @@ public class MainMenu : MonoBehaviour {
 	
 	}
 	void OnGUI(){
+
+		if(mainMenuButtons ==true){
 		if(GUI.Button( new Rect(Screen.width/2,Screen.height/2 -100 ,250,80),startButton, startButtonStyle)){
 			Application.LoadLevel("TutorialLevel");
 			// Insert awesome animation
 		}
+		}
+		if(mainMenuButtons ==true){
 		if(GUI.Button(new Rect(Screen.width/2,Screen.height/2,250,80), instructionsButton,instructionButtonStyle)){
 			instrucButtonPressed = true;
-
-
+			mainMenuButtons = false;
+			}
 		}
 		if(instrucButtonPressed == true){
 			GUI.DrawTexture(new Rect(Screen.width/2,Screen.height/2, 300, 300), instructionsTexture, ScaleMode.ScaleToFit);
-			if(GUI.Button(new Rect(1150,250,20,20), closeInstructionsButton))
+			if(GUI.Button(new Rect(1150,250,20,20), closeInstructionsButton)){
 				instrucButtonPressed = false;
+				mainMenuButtons = true;
+			}
 		}
 
+		if(mainMenuButtons ==true){
 		if(GUI.Button (new Rect(Screen.width/2 +800,Screen.height/2 - 430,140,221),exitButton, exitButtonStyle))
 			Application.Quit();
+		}
 
+		if(mainMenuButtons ==true){
 		if(GUI.Button(new Rect(Screen.width/2,Screen.height/2+100,250,80), optionsButton,optionsButtonStyle)){
 			optionsButtonPressed = true;
+		}
 		}
 		if(optionsButtonPressed == true){
 			GUI.DrawTexture(new Rect(Screen.width/2, Screen.height/2,250,80),optionsTexture, ScaleMode.ScaleToFit);
---
+++ GameJamUnityProject/Assets/Scripts/OptionsScript.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections;
+
+public class OptionsScript : MonoBehaviour {
+
+//The texure to be drawn for the splash screen
+public Texture2D backGroundTexture;
+public Texture2D backButton;
+//width and height of button
+public int buttonWidth=100;
+public int buttonHeight=30;
+//the space between the buttons
+public int buttonSpacing=70;
+//the start Y position of button
+public int buttonYStart=300;
+public GUISkin myskin;
+	public GUISkin bar;
+	public MainMenu options;
+
+	void Start(){
+		GameObject opt = GameObject.Find("Scripts");
+		options = opt.GetComponent<MainMenu>();
+	}
+
+	void OnGUI()
+		{
+		GUI.skin = bar;
+		if(options.mainMenuButtons == false){
+		GUI.Label(new Rect(Screen.width/2-30.0f,230.0f,100.0f,40.0f),"Master Volume:");
+		// Master sound scroll bar
+		GameDataScript.volume = Mathf.RoundToInt(GUI.HorizontalScrollbar(new Rect(Screen.width/2-100.0f,250.0f,300.0f,40.0f),GameDataScript.volume,10,0,100));
+		GUI.Label(new Rect(Screen.width/2-150.0f,250.0f,100.0f,40.0f),GameDataScript.volume.ToString());
+		}
+		//add the "go back" button
+		if (GUI.Button(new Rect(10.0f,10.0f,60.0f,50.0f), " "))
+			{
+			//gameData.GetComponent<GameDataScript>().changeAudio();
+			//goes back to the main menu
+			StartCoroutine(PlayButtonClick());
+			}		
+		}
+	
+	IEnumerator PlayButtonClick()
+		{
+		audio.Play();
+		// Waits end of the clip before moving on
+		yield return new WaitForSeconds(audio.clip.length);
+		// Loads the main menu
+		Application.LoadLevel("MainMenu");
+		}
+
+}
