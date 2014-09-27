ee5d5e155cf9f27eb323af5b11cb717684050b9a
H <hamishzeb91@gmail.com>
2014-09-27 16:45:31 +0200

    Scrollbaaaaaar!

--- GameJamUnityProject/Assets/Scripts/MainMenu.cs
+++ GameJamUnityProject/Assets/Scripts/MainMenu.cs
@@ -46,7 +46,7 @@ public class MainMenu : MonoBehaviour {
 		}
 		if(instrucButtonPressed == true){
 			GUI.DrawTexture(new Rect(Screen.width/2,Screen.height/2, 300, 300), instructionsTexture, ScaleMode.ScaleToFit);
-			if(GUI.Button(new Rect(1150,250,20,20), backButton, backButtonStyle)){
+			if(GUI.Button(new Rect(1150,250,250,80), backButton, backButtonStyle)){
 				instrucButtonPressed = false;
 				mainMenuButtons = true;
 			}
@@ -66,10 +66,10 @@ public class MainMenu : MonoBehaviour {
 			mainMenuButtons = false;
 			GUI.DrawTexture(new Rect(Screen.width/2, Screen.height/2,250,80),optionsTexture, ScaleMode.ScaleToFit);
 			GUI.Label(new Rect(Screen.width/2-30.0f,230.0f,200.0f,80.0f),"Master Volume:");
-				GUI.skin = sideScroll;
-			GameDataScript.volume = Mathf.RoundToInt(GUI.HorizontalScrollbar(new Rect(Screen.width/2-100.0f,250.0f,300.0f,40.0f),GameDataScript.volume,10,0,100));
+			GUI.skin = sideScroll;
+			GameDataScript.volume = Mathf.RoundToInt(GUI.HorizontalScrollbar(new Rect(Screen.width/2-100.0f,250.0f,300.0f,80.0f),GameDataScript.volume,10,0,100));
 			GUI.Label(new Rect(Screen.width/2-150.0f,250.0f,100.0f,40.0f),GameDataScript.volume.ToString());
-			if(GUI.Button(new Rect(1150,250,20,20), backButton, backButtonStyle)){
+			if(GUI.Button(new Rect(1150,250,250,80), backButton, backButtonStyle)){
 				optionsButtonPressed = false;
 				mainMenuButtons = true;
 			}
