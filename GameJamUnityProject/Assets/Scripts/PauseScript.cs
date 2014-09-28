877e9a11de29f6b02994c4c1ba91d01d94087cfe
ricardobarros95 <ricardobarros95@gmail.com>
2014-09-28 14:57:26 +0100

    HappyChanges

--- GameJamUnityProject/Assets/Scripts/PauseScript.cs
+++ GameJamUnityProject/Assets/Scripts/PauseScript.cs
@@ -3,14 +3,23 @@ using System.Collections;
 
 public class PauseScript : MonoBehaviour {
 
-	public enum State { None, Game, Instructions, Options, };
+	public enum State { Pause, Game, Instructions, Options, };
 	public GUIStyle pauseStyle;
 	TextureAnimationScript animScript;
 	public State state;
+	public GUIStyle backButtonStyle;
+	public GUIStyle instructionButtonStyle;
+	public GUIStyle optionsButtonStyle;
+	public Texture2D instructionsTexture;
+	public GUISkin sideScroll;
+	public ScrollTextureAnimation scroll;
+	public GUIStyle masterVolume;
+	public GUIStyle volumeLabel;
 
 	// Use this for initialization
 	void Start () {
-		animScript = gameObject.GetComponent<TextureAnimationScript>();
+		scroll = GameObject.FindGameObjectWithTag("Scroll").GetComponent<ScrollTextureAnimation>();
+		state = State.Game;
 	}
 	
 	// Update is called once per frame
@@ -25,20 +34,33 @@ public class PauseScript : MonoBehaviour {
 		float y = Screen.height / 4;
 		float height = Screen.height / 12;
 		float emptySpace = Screen.height / 25;
-		if (GUI.Button(new Rect(Screen.width -120,Screen.height-700, 120, 70), "", pauseStyle))
+
+		if(state == State.Game){
+			if (GUI.Button(new Rect(x*0.1f, y*0.4f, width*0.7f, height), "", pauseStyle))
 			{
-			animScript.Reset();			
-			if (GUI.Button(new Rect(x, y, width, height), "", instructionButtonStyle))
+				scroll.renderer.enabled = true;
+				//animScript.Reset();
+				state = State.Pause;
+			
+			}
+		}
+		if(scroll.open == false){
+		if(state == State.Pause){
+			if (GUI.Button(new Rect(x*1.15f, y*1.2f, width, height), "", instructionButtonStyle))
 				state = State.Instructions;
 			y += height + emptySpace;
-
-			if (GUI.Button(new Rect(x, y, width, height), "", optionsButtonStyle))
+			
+			if (GUI.Button(new Rect(x*1.15f, y*1.2f, width, height), "", optionsButtonStyle))
 				state = State.Options;
-			y += height + emptySpace;
-
-			if (GUI.Button(new Rect(x, y, width, height), "", exitButtonStyle))
-				Application.LoadLevel(0);
+				y += height + emptySpace;
+			if (GUI.Button(new Rect(x*1.15f, y*1.2f, width, height), "", backButtonStyle)){
+					state = State.Game;
+					scroll.renderer.enabled = false;
+					scroll.Reset();
+				}
 			}
+
+		
 		if(state == State.Instructions)
 		{
 			GUI.DrawTexture(new Rect(Screen.width / 2, Screen.height / 2, 300, 300), instructionsTexture, ScaleMode.ScaleToFit);
@@ -46,20 +68,26 @@ public class PauseScript : MonoBehaviour {
 			if (GUI.Button(new Rect(x, y, width, height), "", backButtonStyle))
 				state = State.Game;
 		}
+		if(scroll.open == false){
 		if(state == State.Options)
 		{
 			GUI.skin = sideScroll;
 			
-			GUI.Label(new Rect(x, y, width, height / 2), "Master Volume:");
+					GUI.Label(new Rect(x*1.15f, y*1.5f, width, height / 2), "Master Volume", masterVolume);
 			y += height / 2;
 			
-			GameDataScript.volume = GUI.HorizontalScrollbar(new Rect(x - width, y - height / 10, width * 3, height * 1.5f), GameDataScript.volume, 1, 0, 100);
-			GUI.Label(new Rect(x - width * 1.1f, y + height / 5, width / 10, height), ((int)GameDataScript.volume).ToString());
+			GameDataScript.volume = GUI.HorizontalScrollbar(new Rect(x - width *(-0.3f), y - height / 2 * (-3.5f), width *1.5f, height * 1.5f), GameDataScript.volume, 1, 0, 100);
+					GUI.Label(new Rect(x - width *(-0.1f), y + height / 2 *3.5f, width / 10, height), ((int)GameDataScript.volume).ToString(),masterVolume);
 			y += height + emptySpace;
 			
-			if (GUI.Button(new Rect(x, y, width, height), "", backButtonStyle))
-				state = State.Main;
+					if (GUI.Button(new Rect(x*1.15f, y *1.5f, width, height), "", backButtonStyle)){
+				state = State.Game;
+				scroll.renderer.enabled = false;
+				scroll.Reset();
+			}
+		}
 		}
 
 	}
+}
 }
